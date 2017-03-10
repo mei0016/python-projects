8964a82b2e437f9de87774cb191c3d168cc105e7
mei0016 <tommei0016@gmail.com>
2017-03-10 18:16:41 +1100

    Add files via upload

---
+++ guessnum.py
@@ -0,0 +1,24 @@
+from random import randint
+import time
+import re
+
+number = (randint(0,100))
+print (number)
+print ("Find the number between 0 and 100\n")
+
+def check():
+  guess = input("Guess a number: ")
+  if re.search('[a-zA-Z]', guess):
+    print ("Try again\n")
+    check()
+  else:
+    if int(guess) > number:
+      print ("Lower\n")
+      check()
+    elif int(guess) < number:
+      print ("Higher\n")
+      check()
+    else:
+      print ("Correct!")
+      time.sleep(3)
+check()
